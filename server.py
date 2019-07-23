2a01cdac26f705e6a5b631e506a9cf1efd4d3331
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-23 16:53:31 -0700

    added options for complement

--- server.py
+++ server.py
@@ -17,14 +17,14 @@ AWESOMENESS = [
 @app.route("/")
 def start_here():
     """Home page."""
-
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    
+    return "<!doctype html><html>Hi! This is the home page. <br><a href=/hello>Go to Hello page</a></html>"
 
 
 @app.route("/hello")
 def say_hello():
     """Say hello and prompt for user's name."""
-
+    
     return """
     <!doctype html>
     <html>
@@ -35,6 +35,14 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <br>
+          <br>
+          <select name="compliment">
+            <option value="">--Please choose an compliment--</option>
+            <option value="awesome">Awesome</option>
+            <option value="terrific">Terrific</option>
+            <option value="fantastic">Fantastic</option>
+          </select>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -47,10 +55,11 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
+    compliment = request.args.get("compliment")
 
-    compliment = choice(AWESOMENESS)
+    # compliment = choice(AWESOMENESS)
 
-    y = x
+    # y = x
 
     return """
     <!doctype html>
@@ -68,4 +77,4 @@ def greet_person():
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
